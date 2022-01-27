b0ac39270f7f6e9ab0632db17788d82c27e7eb02
郭秀峰 <guoxiufeng@bytedance.com>
2022-01-27 18:16:54 +0800

    fix test project

--- packages/@test/mio-vue/src/App.vue
+++ packages/@test/mio-vue/src/App.vue
@@ -1,15 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  <JsxComponent />
-  <el-row>
-    <el-button>默认按钮</el-button>
-    <el-button type="primary">主要按钮</el-button>
-    <el-button type="success">成功按钮</el-button>
-    <el-button type="info">信息按钮</el-button>
-    <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
-  </el-row>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <JsxComponent />
+  </div>
 </template>
 
 <script lang="ts">
